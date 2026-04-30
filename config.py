dce868eb8a00664ceb4a992043a60544c08e1897
Toqa Rabah <toqa.rabah@student.com>
2026-04-30 11:58:49 +0300

    update

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    JWT_SECRET = os.environ.get('JWT_SECRET')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'super-secret-key-2024-for-flask-session')
+    JWT_SECRET = os.environ.get('JWT_SECRET', 'jwt-secret-key-for-token-2024')
     
     # MySQL Workbench Configuration
     MYSQL_HOST = '127.0.0.1'
     MYSQL_PORT = 3306
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''  # <- change this to your MySQL password
+    MYSQL_PASSWORD = ''  
     MYSQL_DATABASE = 'auth_system'
